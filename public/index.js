5b825c5fce502981feeef247804a25422407f8df
rjsoccer11 <59569283+rjsoccer11@users.noreply.github.com>
2025-05-14 01:29:46 +0800

    Update index.js

--- public/index.js
+++ public/index.js
@@ -2,7 +2,6 @@ const socket = io();
 document.getElementById('submitName').onclick = () => {
   const name = document.getElementById('username').value.trim();
   if (name) {
-    localStorage.setItem('username', name); // ✅ Save it
     socket.emit('createUsername', name);
   }
 };
