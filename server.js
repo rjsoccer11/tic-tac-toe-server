3bdd4af9b7e4997c42795c6f1b319ca0a449d3f9
rjsoccer11 <59569283+rjsoccer11@users.noreply.github.com>
2025-05-14 01:34:24 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+const rooms = {};
 
 app.use(express.static('public'));
 
